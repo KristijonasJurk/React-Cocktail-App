3e7431485cc80d18c3bd04825c85ac0abf2018a2
KristijonasJurk <70882614+KristijonasJurk@users.noreply.github.com>
2021-02-24 16:29:23 +0200

    pages setup and about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,9 +2,10 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat adipisci alias eius rerum qui? Repellendus doloribus architecto provident facilis, ducimus quae modi, veniam sapiente accusamus id similique, mollitia odit sit!</p>
+    </section>
   )
 }
 
